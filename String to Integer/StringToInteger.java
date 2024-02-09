110131be288fb445b9a19660c83fe297da25c418
Sowmica <157509411+Sowmica09@users.noreply.github.com>
2024-02-09 22:19:48 +0530

    Create StringToInteger.java

---
+++ String to Integer/StringToInteger.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int myAtoi(String s) {
+     s=s.trim();
+     if(s.isEmpty()) return 0;
+     int i=0 ,sign =1;
+      if(s.charAt(i)=='-'){
+        sign =-1;
+        i++;
+        }else if(s.charAt(i)=='+') i++;
+
+        int ans =0;
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+          int digit = s.charAt(i)-'0';
+        if(ans>(Integer.MAX_VALUE)/10 || (ans==Integer.MAX_VALUE/10 && digit >7)){
+            return (sign==1)?Integer.MAX_VALUE : Integer.MIN_VALUE;
+         }
+          ans=ans*10+digit;
+          i++;
+         }
+         return ans*sign;
+    }
+}
