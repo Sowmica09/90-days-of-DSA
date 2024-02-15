4e0f272188d6b56248f4cca548654c7c1cfbb6a5
Sowmica <157509411+Sowmica09@users.noreply.github.com>
2024-02-15 23:06:27 +0530

    Create SortList.java

---
+++ Sort List/SortList.java
@@ -0,0 +1,51 @@
+class Solution {
+    public static ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast= fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode head1,ListNode head2){
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
+        while(head1!=null && head2!=null){
+            if(head1.val<=head2.val){
+                temp.next=head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next=head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1!=null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2!=null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        //find mid
+         ListNode mid = getMid(head);
+        //left hand & rightHand merge
+        ListNode rightHand = mid.next;
+        mid.next=null;
+        ListNode newLeft = sortList(head);
+        ListNode newRight = sortList(rightHand);
+        //merge
+        return merge(newLeft,newRight);
+    }
+}
