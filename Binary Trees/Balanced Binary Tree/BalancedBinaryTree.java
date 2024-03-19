3ac102a5a779f77dc43ca57472817dd055a91524
Sowmica <157509411+Sowmica09@users.noreply.github.com>
2024-03-19 20:04:21 +0530

    Create BalancedBinaryTree.java

---
+++ Binary Trees/Balanced Binary Tree/BalancedBinaryTree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root)!=-1;
+    }
+    public static int dfsHeight(TreeNode root){
+        if(root == null) return 0;
+        int left = dfsHeight(root.left);
+        if(left==-1) return -1;
+        int right = dfsHeight(root.right);
+        if(right == -1) return -1;
+
+        if(Math.abs(left-right)>1) return -1;
+        return Math.max(left,right)+1;
+    }
+}
